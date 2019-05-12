cc91faf3fd2cb63d4516b50685c0c4a6dba19f4a
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 03:19:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
-1.upto(100) do |i|
- if i%3 == 0 && i%5 == 0 
-   puts "FizzBuzz"
+def fizzbuzz(int)
+  if int %3==0
+    puts "Fizz"
+
+end
+end
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
