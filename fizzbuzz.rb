355f655244fb0d9f32320a98f552a15ad36fd843
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 03:11:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,5 @@
 1.upto(100) do |i|
- if i%3 ==
+ if i%3 == 0 && i%5 == 0 
+   puts "FizzBuzz"
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
