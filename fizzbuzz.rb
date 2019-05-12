3ac3764e52aaa049d2fd157f9c32e268ef1713a8
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 02:34:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,5 @@ fizzbuzz=Integer
   if fizzbuzz%3==0
     puts "Fizz"
 end
-end
   # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
