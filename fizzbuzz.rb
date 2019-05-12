5f943436e437a9be7422ea02e546e7e3492a4f20
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 03:24:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif int % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif int % 3 == 0 
-    puts "Fizz"
+    "Fizz"
   else 
-    return FALSE
+    ""
 end
 end
 
