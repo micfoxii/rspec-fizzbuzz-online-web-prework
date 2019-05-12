4fdc205445b8127f1d7bdb2b2056991006d5d6b0
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 02:44:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,8 @@
-if fizzbuzz 
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+fizz_4 = fizzbuzz(4)
+
+ 
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
