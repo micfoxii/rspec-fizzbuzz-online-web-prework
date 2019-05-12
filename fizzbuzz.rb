fd462b45fcf040a951d18a4fd7541326b87ac976
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-12 02:04:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,7 @@
-# Don't forget! This file needs to be 'required' in its spec file
+fizzbuzz=3
+  if #{"number"}%3==0
+    def fizzbuzz(number=3)
+end
+end
+  # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
